5bb7bbf4d8da84a5467f7c512a498c9d02c9553c
Ben He <Ben@HeBinghuas-MacBook-Pro.local>
2015-07-31 17:58:45 +0800

    commit Day 1 Search APIs

---
+++ 01-Search-APIs/SearchAPIs/FriendViewController.h
@@ -0,0 +1,16 @@
+//
+//  FriendViewController.h
+//  SearchAPIs
+//
+//  Created by HeBinghua on 15/7/31.
+//  Copyright © 2015年 Ben. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class Person;
+
+@interface FriendViewController : UIViewController
+
+@property (nonatomic, strong) Person *person;
+@end
